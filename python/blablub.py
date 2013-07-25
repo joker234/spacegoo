7e3a58e4317a811a813957aae876ce5f85425380
Johannes Visintini <github@joker234.de>
2013-07-26 01:34:44 +0200

    blablub is now a nop-bot

--- python/blablub.py
+++ python/blablub.py
@@ -10,56 +10,70 @@ import data
 import json
 
 def main(config, s):
-    s.connect((config["ip"], config["port"]))
-    io = s.makefile('rw')
-    network.write('login %s %s' % (config["user"], config["password"]), io)
+	s.connect((config["ip"], config["port"]))
+	io = s.makefile('rw')
+	network.write('login %s %s' % (config["user"], config["password"]), io)
 
-    planet_distance = ""
+	planet_distance = ""
 
-    while True:
-        json_data = network.get_data(io)
+	while True:
+		json_data = network.get_data(io)
 
-        if json_data:
-            if data.extract_winner(json_data):
-                exit(0)
+		if json_data:
+			if data.extract_winner(json_data):
+				exit(0)
 
-            if not planet_distance:
-                planet_distance = distance.create_planets_dict(json_data, 4, False)
+			own_id = data.extract_own_id(json_data)
+			my_planets = data.extract_my_planets(json_data, own_id)
+			enemy_planets = data.extract_enemy_planets(json_data, own_id)
+			my_planet_id = data.extract_my_planet_ids(json_data, own_id)
 
-            own_id = data.extract_own_id(json_data)
-            my_planets = data.extract_my_planets(json_data, own_id)
-            enemy_planets = data.extract_enemy_planets(json_data, own_id)
+			print "own_id: " + str(own_id)
+			print "my_planets: " + str(my_planets)
+			print "enemy_planets: " + str(enemy_planets)
 
-            my_planet_id = data.extract_my_planet_ids(json_data, own_id)
+			network.write("nop", io)
 
-            if not my_planets:
-                network.write(("nop"), io)
-            elif not enemy_planets:
-                network.write(("nop"), io)
-            else:
-                planet = random.choice(my_planets)
-                targets = planet_distance[planet["id"]]
 
-                ship_values = data.return_ship_value(planet)
+			## von eeemsi
+			#
+			#if not planet_distance:
+			#    planet_distance = distance.create_planets_dict(json_data, 4, False)
 
-                for i in xrange(len(targets)):
-                    if targets[i][1] not in my_planet_id:
-                        target = targets[i][1]
-                        break
+			#own_id = data.extract_own_id(json_data)
+			#my_planets = data.extract_my_planets(json_data, own_id)
+			#enemy_planets = data.extract_enemy_planets(json_data, own_id)
 
-                network.write("send %s %s %s %s %s" % (planet["id"],
-                    target,
-                    random.randint(ship_values[0][0], ship_values[0][1]),
-                    random.randint(ship_values[1][0], ship_values[1][1]),
-                    random.randint(ship_values[2][0], ship_values[2][1])), io)
+			#my_planet_id = data.extract_my_planet_ids(json_data, own_id)
+
+			#if not my_planets:
+			#    network.write(("nop"), io)
+			#elif not enemy_planets:
+			#    network.write(("nop"), io)
+			#else:
+			#    planet = random.choice(my_planets)
+			#    targets = planet_distance[planet["id"]]
+
+			#    ship_values = data.return_ship_value(planet)
+
+			#    for i in xrange(len(targets)):
+			#        if targets[i][1] not in my_planet_id:
+			#            target = targets[i][1]
+			#            break
+
+			#    network.write("send %s %s %s %s %s" % (planet["id"],
+			#        target,
+			#        random.randint(ship_values[0][0], ship_values[0][1]),
+			#        random.randint(ship_values[1][0], ship_values[1][1]),
+			#        random.randint(ship_values[2][0], ship_values[2][1])), io)
 
 if __name__ == "__main__":
-    file_name = args.receive_argument(argv)
-    config = config.parse_and_return_config(file_name)
+	file_name = args.receive_argument(argv)
+	config = config.parse_and_return_config(file_name)
 
-    config["user"] = "foooooo"
-    config["password"] = "weepgeydgoc5"
+	config["user"] = "foooooo"
+	config["password"] = "weepgeydgoc5"
 
-    s = network.establish_connection(config)
+	s = network.establish_connection(config)
 
-    main(config, s)
+	main(config, s)
